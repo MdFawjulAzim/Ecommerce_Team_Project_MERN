d54c10f47811bb5ab705eca37086eaf7d8e13a41
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2025-01-28 18:20:29 +0600

    file

---
+++ src/models/AddressModel.js
@@ -0,0 +1,86 @@
+import mongoose from "mongoose";
+
+const AddressSchema = new mongoose.Schema(
+  {
+    userID: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    avatar:{
+        type: String,
+        default: "",
+    },
+    cus_add: {
+      type: String,
+      default: "",
+    },
+    cus_city: {
+      type: String,
+      default: "",
+    },
+    cus_city: {
+      type: String,
+      default: "",
+    },
+    cus_country: {
+      type: String,
+      default: "",
+    },
+    cus_fax: {
+      type: String,
+      default: "",
+    },
+    cus_name: {
+      type: String,
+      default: "",
+    },
+    cus_phone: {
+      type: String,
+      default: "",
+    },
+    cus_postcode: {
+      type: String,
+      default: "",
+    },
+    cus_state: {
+      type: String,
+      default: "",
+    },
+    ship_add: {
+      type: String,
+      default: "",
+    },
+    ship_city: {
+      type: String,
+      default: "",
+    },
+    ship_country: {
+      type: String,
+      default: "",
+    },
+    ship_name: {
+      type: String,
+      default: "",
+    },
+    ship_phone: {
+      type: String,
+      default: "",
+    },
+    ship_postcode: {
+      type: String,
+      default: "",
+    },
+    ship_state: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const AddressModel = mongoose.model("address", AddressSchema);
+
+export default AddressModel;
